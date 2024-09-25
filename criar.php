2b6a310f4ef207c9036b15e5e6e90b133b16c6e1
JubsGS <julia.g.serra@unesp.br>
2024-09-25 15:31:56 -0300

    uaaaaaaaaaaaaaaaa

--- criar.php
+++ criar.php
@@ -28,25 +28,19 @@
     </header>
     
     <main class="registro">
-        
         <?php
             include('cabecalho.php');
            
-            
-            
-            
-            
-
             echo"
-            <form action='#' method='POST' enctype='multipart/form-data'>
+            <form action='#' method='POST' enctype='multipart/form-data'
 
             <div class='Dados'>
+                <label><h1>SEJA BEM-VINDO!</h1><br><h2>Criar conta</h2><br></label>
                 Selecione uma foto para seu perfil(*.jpg)<br>
                 <input type='file' name='foto'><br><br>
             </div>
 
             <div class='Dados'>
-                <label><h1>SEJA BEM-VINDO!</h1><br><h2>Criar conta</h2><br></label>
                 <label for='nome'>Nome de usuário</label>
                 <input type='text' id='nome' name='nome' required>
             </div>
@@ -74,6 +68,34 @@
             </div>
 
             </form>";
+
+            if($_POST){
+                $conn=conecta();
+                //insere os valores do formulario em suas respectivas colunas
+                $varSQL="INSERT INTO usuario (nome, email, senha, telefone, admin, excluido)
+                        VALUES (:nome,  :email, :senha, :telefone, false, false)";
+                
+                $insert= $conn->prepare($varSQL);
+        
+                $insert->bindParam(':nome', $_POST["nome"]);
+                $insert->bindParam(':email', $_POST["email"]);
+                $insert->bindParam(':senha', $_POST["senha"]);
+                $insert->bindParam(':telefone', $_POST["telefone"]);
+        
+                if($insert->execute()>0){
+                    echo"<script type='javascript'>alert('Conta criada com sucesso!');";
+                }
+                if($_FILES){
+                    $id=$conn->lastInsertId();
+                    $varArqRecebido=$_FILES['foto']['tmp_name'];
+                    $varExtensaoPadrao='jpg';
+                    $varNovoArq="imagens/u$id.$varExtensaoPadrao";
+                    if(move_uploaded_file($varArqRecebido, $varNovoArq)){
+                       echo"<script type='javascript'>alert('Arquivo de imagem enviado com sucesso!');";
+                    }
+                }
+                
+            }
         ?>
     </main>
     <footer>
@@ -83,39 +105,6 @@
             <li><a href="#">Cookies</a></li>
         </ul>
     </footer>
+        
 </body>
 </html>
-
-<?php
-
-    if($_POST){
-        $conn=conecta();
-        //insere os valores do formulario em suas respectivas colunas
-        $varSQL="INSERT INTO usuario (nome, email, senha, telefone, admin, excluido)
-                VALUES (:nome,  :email, :senha, :telefone, false, false)";
-        
-        $insert= $conn->prepare($varSQL);
-
-        $insert->bindParam(':nome', $_POST["nome"]);
-        $insert->bindParam(':email', $_POST["email"]);
-        $insert->bindParam(':senha', $_POST["senha"]);
-        $insert->bindParam(':telefone', $_POST["telefone"]);
-        $insert->bindParam(':admin', $_POST["admin"]);
-        
-        if($insert->execute()>0){
-            echo "<br><br>Incluído com sucesso";
-
-            if($_FILES){
-                $id=$conn->lastInsertId();
-                $varArqRecebido=$_FILES['foto']['tmp_name'];
-                $varExtensaoPadrao='jpg';
-                $varNovoArq="imagens/u$id.$varExtensaoPadrao";
-                if(move_uploaded_file($varArqRecebido, $varNovoArq)){
-                    echo "<br><br>Arquivo de foto foi recebido com sucesso!";
-                }
-            }
-        }
-    }
-
-    header("location:index.php");
-?>
